e885fc17ca33d66d748e3dcd95a8468e6dee6092
haitang <wxxwtt@163.com>
2021-02-08 15:24:16 +0800

    create login page

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入 ElementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 // 引入全局样式文件
 import './style/index.less'
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/utils/request.js
@@ -0,0 +1,7 @@
+// 请求模块
+import axios from 'axios'
+
+const request = axios.create({
+
+})
+export default request
